18dfd04cbedce1836879fb3412753f54fcaf1589
Choko <choko@curioswitch.org>
2024-05-16 16:18:57 +0900

    Add some explanation to landing page

--- frontend/client/src/pages/index/+Page.tsx
+++ frontend/client/src/pages/index/+Page.tsx
@@ -1,7 +1,9 @@
-import { Button } from "@/components/ui/button";
-import { useAuth } from "@/hooks/firebase";
 import { GithubAuthProvider, signInWithPopup } from "firebase/auth";
 import { useCallback } from "react";
+
+import { Button } from "@/components/ui/button";
+import { H1, P } from "@/components/ui/typography";
+import { useAuth } from "@/hooks/firebase";
 
 export default function Page() {
   const auth = useAuth();
@@ -15,9 +17,37 @@ export default function Page() {
   }, [auth]);
 
   return (
-    <div className="col-span-4 md:col-span-8 lg:col-span-12">
-      <h1>Tasuke</h1>
-      <Button onClick={onSignUpClick}>Sign up with GitHub</Button>
-    </div>
+    <>
+      <div className="col-span-4 md:col-span-8 lg:col-span-12">
+        <H1>Tasuke</H1>
+      </div>
+      <div className="col-span-4 md:col-span-8 lg:col-span-12">
+        <P>
+          tasuke is short for the Japanese 手助け (tedasuke), meaning to give a
+          helping hand. It aims to support OSS developers in giving each other a
+          helping hand by connecting code reviewers to PRs in otherwise
+          unrelated OSS repositories.
+        </P>
+        <P>
+          Everyone knows the{" "}
+          <a
+            href="https://xkcd.com/2347/"
+            className="underline"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            lone maintainer
+          </a>{" "}
+          issue in OSS - let's see if we can make it at least a little less
+          lonely.
+        </P>
+      </div>
+      <div className="col-span-4 md:col-span-8 lg:col-span-12">
+        <P>
+          If you are interested in helping with code reviews, create an account.
+        </P>
+        <Button onClick={onSignUpClick}>Sign up with GitHub</Button>
+      </div>
+    </>
   );
 }
