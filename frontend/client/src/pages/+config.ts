d21fce84565971cbb055bc6de2ac7f560ff563c5
Choko <choko@curioswitch.org>
2024-05-16 15:05:03 +0900

    Scaffold shadcn/ui (#17)

--- frontend/client/src/pages/+config.ts
+++ frontend/client/src/pages/+config.ts
@@ -1,8 +1,8 @@
+import Head from "@/layouts/Head.jsx";
+import Layout from "@/layouts/Layout.jsx";
+import Wrapper from "@/layouts/Wrapper.jsx";
 import vikeReact from "vike-react/config";
 import type { Config } from "vike/types";
-import Head from "../layouts/Head.jsx";
-import Layout from "../layouts/Layout.jsx";
-import Wrapper from "../layouts/Wrapper.jsx";
 
 // Default config (can be overridden by pages)
 export default {
---
+++ frontend/client/src/pages/index/+Page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Page() {
+  return (
+    <div className="col-span-4 md:col-span-8 lg:col-span-12">
+      <h1>Tasuke</h1>
+      <Button>Sign up with GitHub</Button>
+    </div>
+  );
+}
