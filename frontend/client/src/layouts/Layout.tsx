8f25f10c0f18814b96da4c5f697930e12550fcca
Choko <choko@curioswitch.org>
2024-05-17 15:49:18 +0900

    Add user menu and implement sign-out

--- frontend/client/src/layouts/Layout.tsx
+++ frontend/client/src/layouts/Layout.tsx
@@ -1,14 +1,43 @@
 import type React from "react";
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { useFirebaseUser } from "@/hooks/firebase";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useAuth, useFirebaseUser } from "@/hooks/firebase";
+import { type User as FirebaseUser, signOut } from "firebase/auth";
+import { LogOut, User } from "lucide-react";
+import { forwardRef, useCallback } from "react";
+
+const UserAvatar = forwardRef(
+  ({ fbUser, ...props }: { fbUser?: FirebaseUser }, ref) => {
+    return (
+      <Avatar ref={ref} {...props}>
+        <AvatarImage src={fbUser?.photoURL} />
+        <AvatarFallback />
+      </Avatar>
+    );
+  },
+);
 
 export default function Layout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const fbAuth = useAuth();
   const fbUser = useFirebaseUser();
+
+  const onLogOutClick = useCallback(() => {
+    if (!fbAuth || !fbUser) {
+      return;
+    }
+
+    signOut(fbAuth);
+  }, [fbAuth, fbUser]);
 
   return (
     <>
@@ -16,10 +45,25 @@ export default function Layout({
         <div className="container px-6 m-auto">
           <div className="grid grid-cols-4 gap-6 md:grid-cols-8 lg:grid-cols-12">
             <div className="flex justify-end col-span-4 md:col-span-8 lg:col-span-12">
-              <Avatar>
-                <AvatarImage src={fbUser?.photoURL} />
-                <AvatarFallback />
-              </Avatar>
+              {fbUser ? (
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild className="cursor-pointer">
+                    <UserAvatar fbUser={fbUser} />
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent>
+                    <DropdownMenuItem>
+                      <User className="mr-2 h-4 w-4" />
+                      <span>Profile</span>
+                    </DropdownMenuItem>
+                    <DropdownMenuItem onClick={onLogOutClick}>
+                      <LogOut className="mr-2 h-4 w-4" />
+                      <span>Log out</span>
+                    </DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              ) : (
+                <UserAvatar fbUser={fbUser} />
+              )}
             </div>
             {children}
           </div>
