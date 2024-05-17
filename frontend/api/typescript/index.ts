5013962b8dae9b02cab33ce07fd737d785bbc0c4
Choko <choko@curioswitch.org>
2024-05-17 15:13:07 +0900

    Add avatar using firebase user photo (#24)

---
+++ frontend/api/typescript/index.ts
@@ -0,0 +1,2 @@
+export * from "./frontendapi/frontend-FrontendService_connectquery";
+export * from "./frontendapi/frontend_pb";
--- frontend/client/src/layouts/Layout.tsx
+++ frontend/client/src/layouts/Layout.tsx
@@ -1,17 +1,30 @@
 import type React from "react";
 
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { useFirebaseUser } from "@/hooks/firebase";
+
 export default function Layout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const fbUser = useFirebaseUser();
+
   return (
-    <section>
-      <div className="container px-6 m-auto">
-        <div className="grid grid-cols-4 gap-6 md:grid-cols-8 lg:grid-cols-12">
-          {children}
+    <>
+      <section>
+        <div className="container px-6 m-auto">
+          <div className="grid grid-cols-4 gap-6 md:grid-cols-8 lg:grid-cols-12">
+            <div className="flex justify-end col-span-4 md:col-span-8 lg:col-span-12">
+              <Avatar>
+                <AvatarImage src={fbUser?.photoURL} />
+                <AvatarFallback />
+              </Avatar>
+            </div>
+            {children}
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 }
