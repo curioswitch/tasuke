c557b3ce263f79968a69210c7df960a7e955439d
Choko <choko@curioswitch.org>
2024-05-22 09:31:20 +0900

    Begin implementing profile form (#30)

--- frontend/client/src/hooks/rpc/index.tsx
+++ frontend/client/src/hooks/rpc/index.tsx
@@ -1,10 +1,11 @@
 import type { Interceptor } from "@connectrpc/connect";
 import { TransportProvider, useQuery } from "@connectrpc/connect-query";
 import { createConnectTransport } from "@connectrpc/connect-web";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { type User, getUser } from "@tasuke/frontendapi";
 import type { User as FirebaseUser } from "firebase/auth";
 import { useMemo } from "react";
-import { useFirebaseUser } from "../firebase";
+import { useFirebaseState, useFirebaseUser } from "../firebase";
 
 function createFirebaseAuthInterceptor(user: FirebaseUser): Interceptor {
   return (next) => async (request) => {
@@ -27,14 +28,36 @@ export function useUser(): User | undefined {
 export function FrontendServiceProvider({
   children,
 }: { children: React.ReactNode }) {
-  const fbUser = useFirebaseUser();
+  const fbUser = useFirebaseState()?.user;
 
   const transport = useMemo(() => {
     const interceptors = fbUser ? [createFirebaseAuthInterceptor(fbUser)] : [];
     return createConnectTransport({ baseUrl: "/", interceptors });
   }, [fbUser]);
 
+  // Most documentation puts the queryClient at the package level as it's the
+  // easiest way to guarantee the cache is preserved regardless of rendering
+  // loop mistakes, but we get to 1) avoid RPCs until firebase auth is resolved
+  // and 2) easily invalidate the query cache on logout, potentially logging-in
+  // as a different user, so we use useMemo here. The documentation says useMemo
+  // should only be used as an optimization and it's not guaranteed to always
+  // be stable even if the inputs don't change. While in some sense an optimization,
+  // it is also important to not send unnecessary backend calls so we do want this
+  // to be stable. In the unlikely event that useMemo behavior changes to be less
+  // strict in the future, we can write our own stable hook instead.
+  const queryClient = useMemo(() => {
+    return new QueryClient({
+      defaultOptions: {
+        queries: {
+          enabled: !!fbUser,
+        },
+      },
+    });
+  }, [fbUser]);
+
   return (
-    <TransportProvider transport={transport}>{children}</TransportProvider>
+    <QueryClientProvider client={queryClient}>
+      <TransportProvider transport={transport}>{children}</TransportProvider>
+    </QueryClientProvider>
   );
 }
--- frontend/client/src/layouts/Wrapper.tsx
+++ frontend/client/src/layouts/Wrapper.tsx
@@ -1,18 +1,12 @@
 import "./styles.css";
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
 import { FirebaseProvider } from "@/hooks/firebase";
 import { FrontendServiceProvider } from "@/hooks/rpc";
-
-const queryClient = new QueryClient();
 
 export default function Wrapper({ children }: { children: React.ReactNode }) {
   return (
     <FirebaseProvider>
-      <QueryClientProvider client={queryClient}>
-        <FrontendServiceProvider>{children}</FrontendServiceProvider>
-      </QueryClientProvider>
+      <FrontendServiceProvider>{children}</FrontendServiceProvider>
     </FirebaseProvider>
   );
 }
